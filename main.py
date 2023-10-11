c396a44a35153e4f641a63eea9aa5b59c84f813a
Unidad de Sistemas OCAD <sistemas@admisionuni.edu.pe>
2023-10-11 14:58:03 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Imagen Negra
+img = np.zeros((10,10,1),np.uint8)
+
+#Mostramos numeros
+print(img)
+
+#mostramos imagen
+plt.ins
+

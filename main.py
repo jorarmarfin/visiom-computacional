3648c1af450e4dd1bb6aa08949a5e4ebb9195f41
Unidad de Sistemas OCAD <sistemas@admisionuni.edu.pe>
2023-10-12 13:50:45 -0500

    Probando

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Imagen Negra
-img = np.zeros((10,10,1),np.uint8)
+img = np.zeros((10, 10, 1), np.uint8)
+
+#cambiamos algunos pixeles
+img[0, 1] = 30
 
 #Mostramos numeros
 print(img)
 
 #mostramos imagen
-plt.ins
+plt.imshow(img, cmap='gray')
+plt.show()
 
